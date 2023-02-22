b9f21eb5dbb0106063a0a37f4061d14757d6804e
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-22 11:37:50 +0530

    Update testing.groovy

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,39 +1,29 @@
 #!/usr/bin/env groovy
 
 def call(String channel = 'aws-chat-testing', String token, String pass = "success") {
+  
   if(pass == 'success'){
-      slackSend (
-      attachments: [
-          [
-              color: '#00FF00',
-              pretext: 'Notification From Jenkins',
-              title: 'Updates',
-              text: "Build Started\n${env.BUILD_TRIGGER_BY} \nJob Name: ${env.JOB_NAME} ${env.BUILD_NUMBER} \n ",
-              footer: 'Jenkins',
-              footer_icon: 'https://jenkins.io/images/logos/jenkins/256.png'
-          ]
-        ],
-        channel: "${channel}",
-        teamDomain:'testing',
-        tokenCredentialId: "${token}",
-      )
-    }
+    color: '#00FF00'
+  }
   else{
-      slackSend (
-      attachments: [
-          [
-              color: '#FF0000',
-              pretext: 'Notification From Jenkins',
-              title: 'Updates',
-              text: "Build Started\n${env.BUILD_TRIGGER_BY} \nJob Name: ${env.JOB_NAME} ${env.BUILD_NUMBER} \n ",
-              footer: 'Jenkins',
-              footer_icon: 'https://jenkins.io/images/logos/jenkins/256.png'
-          ]
-        ],
-        channel: "${channel}",
-        teamDomain:'testing',
-        tokenCredentialId: "${token}",
-      )
+    color: '#FF0000'
+  }
 
-      }
+  env.BUILD_TRIGGER_BY = "${currentBuild.getBuildCauses()[0].shortDescription} / ${currentBuild.getBuildCauses()[0].userId}" //Fetching the build trigger
+  slackSend (
+      
+  attachments: [
+      [
+      color: color,
+      pretext: 'Notification From Jenkins',
+      title: 'Updates',
+      text: "Build Started\n${env.BUILD_TRIGGER_BY} \nJob Name: ${env.JOB_NAME} ${env.BUILD_NUMBER} \n ",
+      footer: 'Jenkins',
+      footer_icon: 'https://jenkins.io/images/logos/jenkins/256.png'
+      ]
+    ],
+    channel: "${channel}",
+    teamDomain:'testing',
+    tokenCredentialId: "${token}",
+  )
 }
