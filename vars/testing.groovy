1936a02b1844ab285de8df9ebeae94055d396869
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-22 11:21:20 +0530

    Add files via upload

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,5 +1,19 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
-  echo "Hello, ${name}."
+      slackSend (
+      attachments: [
+          [
+              color: '#FFFF00',
+              pretext: 'Notification From Jenkins',
+              title: 'Updates',
+              text: "Build Started\n${env.BUILD_TRIGGER_BY} \nJob Name: ${env.JOB_NAME} ${env.BUILD_NUMBER} \n ",
+              footer: 'Jenkins',
+              footer_icon: 'https://jenkins.io/images/logos/jenkins/256.png'
+          ]
+        ],
+        channel: 'aws-chat-testing',
+        teamDomain:'testing',
+        tokenCredentialId: 'slack-bot-99',
+      )
 }
