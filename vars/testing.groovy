e34283ab4151f66f190bfb059c63646713f1d799
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-22 11:34:20 +0530

    Update testing.groovy

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,10 +1,11 @@
 #!/usr/bin/env groovy
 
-def call(String channel = 'aws-chat-testing', String token) {
+def call(String channel = 'aws-chat-testing', String token, String pass = "success") {
+  if(pass == 'success'){
       slackSend (
       attachments: [
           [
-              color: '#FFFF00',
+              color: '#00FF00',
               pretext: 'Notification From Jenkins',
               title: 'Updates',
               text: "Build Started\n${env.BUILD_TRIGGER_BY} \nJob Name: ${env.JOB_NAME} ${env.BUILD_NUMBER} \n ",
@@ -16,4 +17,23 @@ def call(String channel = 'aws-chat-testing', String token) {
         teamDomain:'testing',
         tokenCredentialId: "${token}",
       )
+    }
+  else{
+      slackSend (
+      attachments: [
+          [
+              color: '#FF0000',
+              pretext: 'Notification From Jenkins',
+              title: 'Updates',
+              text: "Build Started\n${env.BUILD_TRIGGER_BY} \nJob Name: ${env.JOB_NAME} ${env.BUILD_NUMBER} \n ",
+              footer: 'Jenkins',
+              footer_icon: 'https://jenkins.io/images/logos/jenkins/256.png'
+          ]
+        ],
+        channel: "${channel}",
+        teamDomain:'testing',
+        tokenCredentialId: "${token}",
+      )
+
+      }
 }
