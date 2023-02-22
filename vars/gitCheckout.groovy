08e611d6dcd778979a2d671f1a94a38138d4fc00
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-22 18:20:01 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,24 +1,13 @@
 #!/usr/bin/env groovy
 
-// def call(String gitUrl, String branch = 'staging') {
-//   echo "Hello Wrolds"
+def call(String gitUrl, String branch = 'staging', String token = " ") {
+  echo "Hello Wrolds"
   
-//   checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], userRemoteConfigs: [[url: "${gitUrl}"]]])
+  checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], userRemoteConfigs: [[url: "${gitUrl}", credentialsId: "${token}"]]])
     
-//    def gitBaseUrl = sh(returnStdout: true, script: 'git config --get remote.origin.url | sed "s/.git$/""/"').trim()  //Grep the git url
-//    def gitCommitLink = sh(returnStdout: true, script: 'git log -n 1 --pretty=format:"%H"').trim()                // Grep the recent commit
-//    env.url= "${gitBaseUrl}/commit/${gitCommitLink}"                                                                 // Mergning bot the details
-//    env.BUILD_TRIGGER_BY = "${currentBuild.getBuildCauses()[0].shortDescription} / ${currentBuild.getBuildCauses()[0].userId}" //Fetching the build trigger
-//    env.commit_details = sh (returnStdout: true, script: '''set +e && git log --format="short" -1 | tail -n +2 ''')       
-// }
-//             credentialsId: "${token}", 
-
-
-def call(Map stageParams) {
-
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
-  }
+   def gitBaseUrl = sh(returnStdout: true, script: 'git config --get remote.origin.url | sed "s/.git$/""/"').trim()  //Grep the git url
+   def gitCommitLink = sh(returnStdout: true, script: 'git log -n 1 --pretty=format:"%H"').trim()                // Grep the recent commit
+   env.url= "${gitBaseUrl}/commit/${gitCommitLink}"                                                                 // Mergning bot the details
+   env.BUILD_TRIGGER_BY = "${currentBuild.getBuildCauses()[0].shortDescription} / ${currentBuild.getBuildCauses()[0].userId}" //Fetching the build trigger
+   env.commit_details = sh (returnStdout: true, script: '''set +e && git log --format="short" -1 | tail -n +2 ''')       
+}
