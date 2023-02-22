46f414b886f9589f96ef668e202febf6bcb0304e
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-22 18:05:29 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,14 +1,31 @@
 #!/usr/bin/env groovy
 
-def call(String gitUrl, String branch = 'staging') {
-  echo "Hello Wrolds"
+// def call(String gitUrl, String branch = 'staging') {
+//   echo "Hello Wrolds"
   
-  checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], userRemoteConfigs: [[url: "${gitUrl}"]]])
+//   checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], userRemoteConfigs: [[url: "${gitUrl}"]]])
     
-   def gitBaseUrl = sh(returnStdout: true, script: 'git config --get remote.origin.url | sed "s/.git$/""/"').trim()  //Grep the git url
-   def gitCommitLink = sh(returnStdout: true, script: 'git log -n 1 --pretty=format:"%H"').trim()                // Grep the recent commit
-   env.url= "${gitBaseUrl}/commit/${gitCommitLink}"                                                                 // Mergning bot the details
-   env.BUILD_TRIGGER_BY = "${currentBuild.getBuildCauses()[0].shortDescription} / ${currentBuild.getBuildCauses()[0].userId}" //Fetching the build trigger
-   env.commit_details = sh (returnStdout: true, script: '''set +e && git log --format="short" -1 | tail -n +2 ''')       
-}
+//    def gitBaseUrl = sh(returnStdout: true, script: 'git config --get remote.origin.url | sed "s/.git$/""/"').trim()  //Grep the git url
+//    def gitCommitLink = sh(returnStdout: true, script: 'git log -n 1 --pretty=format:"%H"').trim()                // Grep the recent commit
+//    env.url= "${gitBaseUrl}/commit/${gitCommitLink}"                                                                 // Mergning bot the details
+//    env.BUILD_TRIGGER_BY = "${currentBuild.getBuildCauses()[0].shortDescription} / ${currentBuild.getBuildCauses()[0].userId}" //Fetching the build trigger
+//    env.commit_details = sh (returnStdout: true, script: '''set +e && git log --format="short" -1 | tail -n +2 ''')       
+// }
 //             credentialsId: "${token}", 
+
+
+def call(String repoUrl, String branch = 'master', String credentialId = '') {
+    def scmVars = [
+        $class: 'GitSCM',
+        userRemoteConfigs: [[
+            credentialsId: credentialId,
+            url: repoUrl
+        ]],
+        branches: [[name: "*/${branch}"]],
+        extensions: [
+            [$class: 'CleanCheckout'],
+            [$class: 'CloneOption', noTags: true, shallow: true, depth: 1]
+        ]
+    ]
+
+    checkout(scmVars)
