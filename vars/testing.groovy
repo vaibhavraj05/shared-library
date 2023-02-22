515d8ea47eb9d567a65cc9d22943ccaab975024f
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-22 11:42:40 +0530

    Update testing.groovy

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -16,7 +16,7 @@ def call(String channel = 'aws-chat-testing', String token, String pass = "succe
       
   attachments: [
       [
-      color: color,
+        color: "${color}",
       pretext: 'Notification From Jenkins',
       title: 'Updates',
       text: "${status}\n${env.BUILD_TRIGGER_BY} \nJob Name: ${env.JOB_NAME} ${env.BUILD_NUMBER} \n ",
