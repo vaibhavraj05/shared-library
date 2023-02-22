15ce3b3520b93c7bf45c6ceeb05f31fc46c08977
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-22 18:02:47 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,21 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(String gitUrl, String branch = 'staging', String token = '') {
+def call(String gitUrl, String branch = 'staging') {
   echo "Hello Wrolds"
   echo "${token}"
-  def scm = [
-            $class: 'GitSCM', 
-            branches: [[name: "*/${branch}"]], 
-            doGenerateSubmoduleConfigurations: false, 
-            extensions: [], 
-            submoduleCfg: [], 
-            userRemoteConfigs: [[
-              credentialsId: "${token}", 
-              url: "${gitUrl}"
-                ]]
-  ]
-  echo "$scm"
-  checkout("${scm}")
+  
+  checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], userRemoteConfigs: [[url: "${gitUrl}"]]])
     
    def gitBaseUrl = sh(returnStdout: true, script: 'git config --get remote.origin.url | sed "s/.git$/""/"').trim()  //Grep the git url
    def gitCommitLink = sh(returnStdout: true, script: 'git log -n 1 --pretty=format:"%H"').trim()                // Grep the recent commit
@@ -23,3 +12,4 @@ def call(String gitUrl, String branch = 'staging', String token = '') {
    env.BUILD_TRIGGER_BY = "${currentBuild.getBuildCauses()[0].shortDescription} / ${currentBuild.getBuildCauses()[0].userId}" //Fetching the build trigger
    env.commit_details = sh (returnStdout: true, script: '''set +e && git log --format="short" -1 | tail -n +2 ''')       
 }
+//             credentialsId: "${token}", 
