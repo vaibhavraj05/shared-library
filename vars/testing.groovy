6495f327ba2a3e034ad2cbe326b0d7e1560f7bd0
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-22 18:12:28 +0530

    Update testing.groovy

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,4 +1,4 @@
-def call(String channel = 'aws-chat-testing', String token, String pass = "success") {
+def call(String channel = 'aws-chat-testing', String token,String pass = "success", String footer = 'Jenkins', String pretext = "Notification FRom Jenkins", String text = "Build Successfull" , String footer_icon = 'https://jenkins.io/images/logos/jenkins/256.png') {
   def color= '#00FF00'
   def status = "Build Successfully"
   if(pass != 'success'){
@@ -11,7 +11,7 @@ def call(String channel = 'aws-chat-testing', String token, String pass = "succe
       
   attachments: [
       [
-        color: "${color}",
+      color: "${color}",
       pretext: 'Notification From Jenkins',
       title: 'Updates',
       text: "${status}\n${env.BUILD_TRIGGER_BY} \nJob Name: ${env.JOB_NAME} ${env.BUILD_NUMBER} \n ",
