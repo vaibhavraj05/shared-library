c3f110aa288aef4914a5c463f7359b60cf10ef31
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-22 12:59:55 +0530

    Update curl.groovy

--- vars/curl.groovy
+++ vars/curl.groovy
@@ -16,7 +16,7 @@ def call(String webhook, String pass = "success") {
                   "pretext": "Notification From Jenkins",
                   "title": Updates,
                   "title_link": "https://jenkins.gkmit.co/view/accel/job/accel-staging/",
-                  "text": ${status}\n${BUILD_TRIGGER_BY} \nJob Name: ${env.JOB_NAME} ${env.BUILD_NUMBER} \n Build Output: (<${env.BUILD_URL}/console|Open>) \n Commit URL (<${url}|Open>),
+                  "text": "${status}\n${BUILD_TRIGGER_BY} \nJob Name: ${env.JOB_NAME} ${env.BUILD_NUMBER} \n Build Output: (<${env.BUILD_URL}/console|Open>) \n Commit URL (<${url}|Open>)",
                   "footer": "testing",
                   "footer_icon": "https://media.licdn.com/dms/image/C560BAQGIOZDvliUyqA/company-logo_200_200/0/1637701111354?e=1684972800&v=beta&t=o4C4cxZu226JNl4Td4GPqOct6jbqi_GUnIuC0SlSEgc",
                   "color": "#006400"
@@ -25,5 +25,5 @@ def call(String webhook, String pass = "success") {
   }' ${webhook}
   """
   echo "${objects}"
-  sh "curl -X POST -H 'Content-type: application/json' --data ${objects}"
+  sh "curl -X POST -H 'Content-type: application/json' --data ${objects} "
 }
