fb9fad27cabd294a28b90a86abf24ad0a615330a
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-23 14:45:06 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,5 +2,10 @@
 
 def call(String gitUrl, String branch = 'staging', String token = " ") {
    checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], userRemoteConfigs: [[url: "${gitUrl}", credentialsId: "${token}"]]])
+   def gitBaseUrl = sh(returnStdout: true, script: 'git config --get remote.origin.url | sed "s/.git$/""/"').trim()  //Grep the git url
+   def gitCommitLink = sh(returnStdout: true, script: 'git log -n 1 --pretty=format:"%H"').trim()                // Grep the recent commit
+   env.url= "${gitBaseUrl}/commit/${gitCommitLink}"                                                                 // Mergning bot the details
+   env.BUILD_TRIGGER_BY = "${currentBuild.getBuildCauses()[0].shortDescription} / ${currentBuild.getBuildCauses()[0].userId}" //Fetching the build trigger
+   env.commit_details = sh (returnStdout: true, script: '''set +e && git log --format="short" -1 | tail -n +2 ''')       
    
 }
