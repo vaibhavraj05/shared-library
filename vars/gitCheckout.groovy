f7692035bbb86d49cbded783c055527f5332f178
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-23 16:50:04 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -4,8 +4,9 @@ def call(String gitUrl, String branch = 'staging', String token = " ") {
    checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], userRemoteConfigs: [[url: "${gitUrl}", credentialsId: "${token}"]]])
    def gitBaseUrl = sh(returnStdout: true, script: 'git config --get remote.origin.url | sed "s/.git$/""/"').trim()  //Grep the git url
    def gitCommitLink = sh(returnStdout: true, script: 'git log -n 1 --pretty=format:"%H"').trim()                // Grep the recent commit
-   url= "${gitBaseUrl}/commit/${gitCommitLink}"                                                                 // Mergning bot the details
+   def commitUrl= "${gitBaseUrl}/commit/${gitCommitLink}"                                                                 // Mergning bot the details
 //    env.BUILD_TRIGGER_BY = "${currentBuild.getBuildCauses()[0].shortDescription} / ${currentBuild.getBuildCauses()[0].userId}" //Fetching the build trigger
-   commit_details = sh (returnStdout: true, script: '''set +e && git log --format="short" -1 | tail -n +2 ''')       
+   def commitDetails = sh (returnStdout: true, script: '''set +e && git log --format="short" -1 | tail -n +2 ''')       
+   return [commitUrl: commitUrl,commitDetails: commitDetails]
    
 }
