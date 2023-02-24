06601e1b306094defe38a4a38f1d5eff501f6c29
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-24 15:39:13 +0530

    Update secret.groovy

--- vars/secret.groovy
+++ vars/secret.groovy
@@ -4,7 +4,7 @@ def call(Map config){
   def secretToken = secret.secretToken ?:error("No token Provided")
   def secretRegion = secret.secretRegion ?: error("No region provided")
   def secretName = secret.secretName ?: error("No secret name provided")
-  withCredentials([string(credentialsId: "${secretToken}", variable: 'accel')]) {
+  withCredentials([string(credentialsId: secretToken, variable: 'accel')]) {
     sh"""
                 /bin/bash -c "export $accel && aws secretsmanager get-secret-value --region ${secretRegion} --secret-id ${secretName} > data"
                 cat data | jq .SecretString | jq --raw-output . | jq -r 'to_entries|map("\\(.key)=\\(.value|tostring)")|.[]' > .env 
