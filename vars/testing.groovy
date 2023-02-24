8da298801cf44c3ac38bde3cc4db53be08b1cfe6
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-24 15:12:55 +0530

    Update testing.groovy

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,18 +1,18 @@
 def call(Map config) {
   
-  def channel = config.channel ?: 'aws-chat-testing'
-  def token = config.token ?: error('No token provided')
-  def message = config.message ?: error('No message provided')
-  def pass = config.pass ?: 'success'
+  def channel = config.channel ?: error('No channel provided')
+  def slackToken = config.token ?: error('No token provided')
+  def message = config.message ?: ''
+  def pass = config.pass ?: 'S'
   def footer = config.footer ?: 'Jenkins'
   def pretext = config.pretext ?: 'Jenkins'
-  def footer_icon = config.footer_icon ?: 'https://jenkins.io/images/logos/jenkins/256.png'
+  def footerIcon = config.footer_icon ?: 'https://jenkins.io/images/logos/jenkins/256.png'
   def teamDomain = config.teamDomain ?: 'testing'
   
 //   String channel, String token, String message, String pass = "success", String footer = 'Jenkins', String pretext = "Jenkins" , String footer_icon = 'https://jenkins.io/images/logos/jenkins/256.png', String teamDomain = "testing"
   def color= '#00FF00'
   def status = "Build Successfully"
-  if(pass != 'success'){
+  if(pass != 'S'){
     color= '#FF0000'
     status = "Build Failed"
   }
@@ -28,11 +28,11 @@ def call(Map config) {
       title: 'Updates',
         text: "${status}\n${env.BUILD_TRIGGER_BY} \nJob Name: ${env.JOB_NAME} ${env.BUILD_NUMBER} \n Build Output: (<${env.BUILD_URL}/console|Open>) \n${message}",
         footer: "${footer}",
-       footer_icon: "${footer_icon}"
+       footer_icon: "${footerIcon}"
       ]
     ],
     channel: "${channel}",
     teamDomain:"${teamDomain}",
-    tokenCredentialId: "${token}",
+    tokenCredentialId: "${slackToken}",
   )
 }
