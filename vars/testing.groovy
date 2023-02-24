615d4ca7cdd6b97fdedf9c377ec9a79d1bb2ce34
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-24 12:46:32 +0530

    Update testing.groovy

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,4 +1,4 @@
-def call(String channel = 'aws-chat-testing', String token,String pass = "success", String footer = 'Jenkins', String pretext = "Jenkins" , String footer_icon = 'https://jenkins.io/images/logos/jenkins/256.png', String teamDomain = "testing") {
+def call(String channel = 'aws-chat-testing', String token,String pass = "success", String = message,String footer = 'Jenkins', String pretext = "Jenkins" , String footer_icon = 'https://jenkins.io/images/logos/jenkins/256.png', String teamDomain = "testing") {
   def color= '#00FF00'
   def status = "Build Successfully"
   if(pass != 'success'){
@@ -14,7 +14,7 @@ def call(String channel = 'aws-chat-testing', String token,String pass = "succes
       color: "${color}",
       pretext: "Notification From ${pretext}",
       title: 'Updates',
-        text: "${status}\n${env.BUILD_TRIGGER_BY} \nJob Name: ${env.JOB_NAME} ${env.BUILD_NUMBER} \n Build Output: (<${env.BUILD_URL}/console|Open>) \n Commit URL (<${env.url}|Open>)\n Commit Details ->\n ${env.commit_details}",
+        text: "${status}\n${env.BUILD_TRIGGER_BY} \nJob Name: ${env.JOB_NAME} ${env.BUILD_NUMBER} \n Build Output: (<${env.BUILD_URL}/console|Open>) \n  ${message}",
         footer: "${footer}",
        footer_icon: "${footer_icon}"
       ]
