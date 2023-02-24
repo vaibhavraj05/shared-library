a3d9941ff81e796cea7166a7da9e20ad8b5dc20a
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2023-02-24 15:36:12 +0530

    Update secret.groovy

--- vars/secret.groovy
+++ vars/secret.groovy
@@ -1,12 +1,15 @@
 #!/usr/bin/env groovy
 
 def call(Map config){
-  withCredentials([string(credentialsId: 'yocharge-secret-manager', variable: 'accel')]) {
-            sh'''
-                /bin/bash -c 'export $accel && aws secretsmanager get-secret-value --region ap-south-1 --secret-id yocharge-client-test-admin > data'
+  def secretToken = secret.secretToken ?:error("No token Provided")
+  def secretRegion = secret.secretRegion ?: error("No region provided")
+  def secretName = secret.secretName ?: error("No secret name provided")
+  withCredentials([string(credentialsId: "${secretToken}", variable: 'accel')]) {
+    sh"""
+                /bin/bash -c "export $accel && aws secretsmanager get-secret-value --region ${secretRegion} --secret-id ${secretName} > data"
                 cat data | jq .SecretString | jq --raw-output . | jq -r 'to_entries|map("\\(.key)=\\(.value|tostring)")|.[]' > .env 
-            '''
-            sh 'rm -f data'
-            sh "cat .env"
+            """
+    sh 'rm -f data'
+    sh "cat .env"
         }
 }
