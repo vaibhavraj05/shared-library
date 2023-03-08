3b3f7ddc61d0cea12a1aaad2333d08c6a6c1351f
vaibhavraj05 <117675608+vaibhavraj05@users.noreply.github.com>
2023-03-08 13:52:30 +0530

    Update DependencyTrack.groovy

--- vars/DependencyTrack.groovy
+++ vars/DependencyTrack.groovy
@@ -16,7 +16,7 @@ def call(Map config){
     if("${package_file}" != ''){
          def pwd = sh(returnStdout: true, script:"pwd")
         echo "${pwd}"
-        sh "docker run --rm -v ${pwd}:/src -w /src node:${nodeversion} npm --prefix ${packagelocation} install"
+        echo "docker run --rm -v ${pwd}:/src -w /src node:${nodeversion} npm --prefix ${packagelocation} install"
         sh 'docker run --rm -v $(pwd):/src cyclonedx/cyclonedx-node /src/"${packagelocation}"'
     }
     else {
