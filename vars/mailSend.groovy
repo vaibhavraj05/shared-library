4f4ce43dce6bc3f528f508af1592f943ec124e19
vaibhavraj05 <vaibhavraj@gkmit.co>
2023-03-09 10:58:15 +0530

    Updates

--- vars/mailSend.groovy
+++ vars/mailSend.groovy
@@ -6,7 +6,7 @@ def call(Map config){
     def mailFrom = config.mailFrom ?: "vaibhavraj@gkmit.co"
     def message =  config.message ?: " "
     def subject = config.subject ?: "Project Detatils"
-    withCredentials([string(credentialsId: "${gmailCred}", variable: 'gmailTemplate')]) {
+    withCredentials([string(credentialsId: 'test-email-temp', variable: 'gmailTemplate')]) {
     echo "I am here"
     sh "set -x;curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd --mail-from ${mailFrom} --mail-rcpt ${groupMail} -F text=${message} --user ${gmailTemplate} --header Subject: ${subject}"
     }
