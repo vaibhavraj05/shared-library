bce19f5fa649d1b25d224c9f93dfd23e9c251e31
vaibhavraj05 <117675608+vaibhavraj05@users.noreply.github.com>
2023-04-03 22:39:40 +0530

    pythonTest

---
+++ vars/pythonTest.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/env groovy
+
+def call(Map config){
+    def data = sh (returnStdout: true, script: '''cat Dockerfile | grep FROM | head -n 1 | cut -d ":" -f 2| cut -d " " -f 1''').trim()
+    try {
+        def setup = sh (returnStdout: false, script:"docker run --rm -v ${workspace}:/app -w /app --name python --env-file=.env --network common python:${data} /bin/bash -c 'pip3 install -r requirements.txt && pytest -rp --junit-xml=test-report.xml >/dev/null 2>&1'")
+    }catch(e){
+        echo ""
+    }
+    junit 'test-report.xml'
+    sh "rm -f .env"
+
+    if(currentBuild.resultIsBetterOrEqualTo('UNSTABLE') || config.pass == "U")
+    {
+        return "U"
+    }
+    return "S"
+    
+}
