f45816cef55be5c35f7f0893e17765e3f2172768
sheershjain <114575647+sheershjain@users.noreply.github.com>
2023-04-03 15:42:02 +0530

    Update terraform-cli.groovy

--- vars/terraform-cli.groovy
+++ vars/terraform-cli.groovy
@@ -7,7 +7,7 @@ def call(Map config){
     def awsRegion = config.awsRegion ?: error("No region provided")           // Region of aws
     def workDir = config.workDir ?: error("No working Directory provided")    // workdir in container
     def terraformDockerImage = "msshahanshah/tools:terrform02"                // Docker image with terrafrom configuration
-    def action = config.action ?: error("No action provided")                 // Action we want to perfrom [init, plan,apply,destroy]
+    def cliAction = config.cliAction ?: error("No action provided")                 // Action we want to perfrom [init, plan,apply,destroy]
 
     withCredentials([string(credentialsId: "${secretToken}", variable: "setup")]) {
                 def AWS_CREDENTIALS = sh (
@@ -16,6 +16,6 @@ def call(Map config){
                     """,
                     returnStdout: true
                 ).trim()
-         sh "set +x; docker run --rm ${AWS_CREDENTIALS} -e AWS_PROFILE=${awsProfile} -e AWS_REGION=${awsRegion} -v ${workspace}/:/code -w /code/${workDir} ${terraformDockerImage} /bin/bash -c '${action}'"           
+         sh "set +x; docker run --rm ${AWS_CREDENTIALS} -e AWS_PROFILE=${awsProfile} -e AWS_REGION=${awsRegion} -v ${workspace}/:/code -w /code/${workDir} ${terraformDockerImage} /bin/bash -c '${cliAction}'"           
         }
 }
