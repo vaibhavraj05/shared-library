33504b371bf87357efd7999c3f291bf2e30c1ebd
vaibhavraj05 <117675608+vaibhavraj05@users.noreply.github.com>
2023-04-03 23:06:11 +0530

    Update nodeTest.groovy

--- vars/nodeTest.groovy
+++ vars/nodeTest.groovy
@@ -4,6 +4,7 @@ def call(Map config){
     def data = sh (returnStdout: true, script: '''cat Dockerfile | grep FROM | head -n 1 | cut -d ":" -f 2| cut -d " " -f 1''').trim()
     sh "docker run --rm -v ${workspace}:/app -w /app node:${data} npm install"
     def result = sh (returnStatus: true, script: "docker run --rm -v ${workspace}:/app -w /app node:${data} /bin/bash -c 'CI=true npm run test:report'")
+    junit 'test-report.xml'
     if(currentBuild.resultIsBetterOrEqualTo('UNSTABLE') || config.pass == "U")
     {
         return "U"
