220c385db98fe075f7131d667a51897050d793a3
vaibhavraj05 <117675608+vaibhavraj05@users.noreply.github.com>
2023-04-04 15:23:37 +0530

    Hot fix

--- vars/nodeTest.groovy
+++ vars/nodeTest.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 
 def call(Map config){
-    def npmversion = "6.14.15"
+    def extraCommand = config.extraCommand ?: "echo"
     def data = sh (returnStdout: true, script: '''cat Dockerfile | grep FROM | head -n 1 | cut -d ":" -f 2| cut -d " " -f 1''').trim()
-    sh "docker run --rm -v ${workspace}:/app -w /app node:${data} npm install -g npm@${npmversion} && npm install"
+    sh "docker run --rm -v ${workspace}:/app -w /app node:${data} ${extraCommand} && npm install"
     def result = sh (returnStatus: true, script: "docker run --rm -v ${workspace}:/app -w /app node:${data} /bin/bash -c 'CI=true npm run test:report'")
     junit 'test-report.xml'
     if(currentBuild.resultIsBetterOrEqualTo('UNSTABLE') || config.pass == "U")
