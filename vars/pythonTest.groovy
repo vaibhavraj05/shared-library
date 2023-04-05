a013127d3c7e28c0e66774400da2440dd7ff5c40
vaibhavraj05 <117675608+vaibhavraj05@users.noreply.github.com>
2023-04-05 17:31:05 +0530

    Update pythonTest.groovy

--- vars/pythonTest.groovy
+++ vars/pythonTest.groovy
@@ -3,7 +3,7 @@
 def call(Map config){
     def data = sh (returnStdout: true, script: '''cat Dockerfile | grep FROM | head -n 1 | cut -d ":" -f 2| cut -d " " -f 1''').trim()
     try {
-        def setup = sh (returnStdout: false, script:"docker run --rm -v ${workspace}:/app -w /app --name python --env-file=.env --network common python:${data} /bin/bash -c 'pip3 install -r requirements.txt && pytest -rp --junit-xml=test-report.xml >/dev/null 2>&1'")
+        def setup = sh (returnStdout: false, script:"docker run --rm -v ${workspace}:/app -w /app --name python --env-file=.env --network common python:${data} /bin/bash -c 'pip3 install -r requirements.txt && pytest -rp --junit-xml=test-report.xml >/dev/null 2>&1 && coverage xml'")
     }catch(e){
         echo ""
     }
